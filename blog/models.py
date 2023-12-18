2a2a1eee5bd9253362496bc1d2f11673e0c14c80
xoos0420 <xoos0420@gmail.com>
2023-12-18 17:48:10 +0900

    231218_1747_mkLandingpage

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,12 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # author : 추가 예정
     
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+    
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
+
+def single_post_pages(request, pk):
+    post = Post.objects.get(pk = pk)
+    
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post,
+        }
+    )
