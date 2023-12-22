24cdb08eba8e5b0abee9d1a3bc1790ce1af358f9
xoos0420 <xoos0420@gmail.com>
2023-12-22 14:23:57 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -97,22 +97,25 @@ class TestView(TestCase):
         # 포스트가 없는 경우
         Post.objects.all().delete()
         self.assertEqual(Post.objects.count(), 0)
-        # 2023-12-21 17:54 작성중
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main_area')
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
     
     def test_post_detail(self):
         # 1.1 Post가 하나 있다.
-        post_001 = Post.objects.create(
+        post_004 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content = 'Hello World. We are the world.',
             author = self.user_trump
         )
         
         # 1.2 그 포스트의 url은 blog/1/ 이다
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(post_004.get_absolute_url(), '/blog/4/')
         
         # 2. 첫 번째 포스트의 상세 페이지 테스트
         # 2.1 첫 번째 post url로 접근하면 정상적으로 작동(status cod:200)
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(post_004.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         
@@ -120,15 +123,15 @@ class TestView(TestCase):
         self.navbar_test(soup)
         
         # 2.3 첫 번째 포스트의 제목(title)이 웹 브라우저 탭 타이틀에 들어있음
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(post_004.title, soup.title.text)
         
         # 2.4 첫 번째 포스트의 제목이 포스트 영역(post_area)에 있다
         main_area = soup.find('div', id = 'main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_004.title, post_area.text)
         
         # 2.5 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다
         self.assertIn(self.user_trump.username.upper(), post_area.text)
         
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(post_004.content, post_area.text)
