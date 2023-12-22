adda36cea7d89c1c5af0400a4a31d6c4de063ce5
xoos0420 <xoos0420@gmail.com>
2023-12-22 16:08:14 +0900

    로그인 한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category
 
 class PostList(ListView):
@@ -41,6 +42,18 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         
         return context
+
+class PostCreate(LoginRequiredMixin ,CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
     
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
