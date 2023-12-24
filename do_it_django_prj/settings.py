d02e4c22c5bed420802922c6114c483d4a4d6fa9
xoos0420 <xoos0420@gmail.com>
2023-12-25 01:19:13 +0900

    포스트 수정 기능 추가

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     
+    'crispy_forms',
+
     'blog',
     'single_pages'
 ]
@@ -127,6 +129,8 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
