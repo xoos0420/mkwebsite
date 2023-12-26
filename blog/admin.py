78280862055df9ae1aa87c045afd263c7d52c8cf
xoos0420 <xoos0420@gmail.com>
2023-12-26 14:20:51 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
