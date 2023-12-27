46dd9a6c13c2bf7198dd6da170d84123a427ea29
xoos0420 <xoos0420@gmail.com>
2023-12-27 12:04:26 +0900

    pagination으로 포스트 나누어 보여주기

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from django.core.exceptions import PermissionDenied
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
