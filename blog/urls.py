d062df03e548e5dbfbce51083d4d021bfb53e5fa
xoos0420 <xoos0420@gmail.com>
2023-12-27 11:51:38 +0900

    댓글 삭제 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -91,6 +91,15 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
 
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
     form_class = CommentForm
